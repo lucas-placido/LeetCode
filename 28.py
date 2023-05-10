0175309201a10acbd1285d0be95121474784bf6e
lucas <lucas.cplacido@hotmail.com>
2023-05-10 13:31:14 -0300

    28. Find the Index of the First Occurrence in a String

--- 28.py
+++ 28.py
@@ -1,15 +1,14 @@
 class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        if needle not in haystack:
-            return -1                
+    def strStr(self, haystack: str, needle: str) -> int:            
         i = 0
         n = len(haystack) -1
         while i <= n:                
             if needle == haystack[i: i + len(needle)]:
-                return i                           
-            i += 1        
+                return i
+            else:                           
+                i += 1        
+        return -1
     
-
 haystack = "sadbutsad"
 needle = "sad"    
 
