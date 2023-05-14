ecd9d0e895e6130cbc7d0461fdca85734c3361a9
lucas <lucas.cplacido@hotmail.com>
2023-05-14 12:14:53 -0300

    238. Product of Array Except Self

--- 238.py
+++ 238.py
@@ -2,21 +2,14 @@ from typing import List
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:                
-        answer = [1 for i in range(len(nums))]
+        answer = [1] * len(nums)
         val = 1
-        for i in range(len(nums)):                                    
-            if i == 0:
-                pass
-            else:
-                answer[i] = nums[i-1] * answer[i-1]
+        for i in range(1, len(nums)):                                                
+            answer[i] = nums[i-1] * answer[i-1]
                 
-        for i in range(-1, -1 -len(nums), -1):            
-            if i == -1:
-                pass
-            else:                
-                val *= nums[i + 1]
-                answer[i] *= val                        
-
+        for i in range(-2, -1 -len(nums), -1):                           
+            val *= nums[i + 1]
+            answer[i] *= val                        
         return answer
     
 nums = [1,2,3,4]    
